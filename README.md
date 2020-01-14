c10672bb433f8ed98da7b4bf7fad8f321f85a5bb
Boundary Effect <boundary.effect@gmail.com>
2020-01-14 01:05:27 +0100

    initial revision

---
+++ README.md
@@ -0,0 +1,15 @@
+Pname+
+======
+
+## Open Play Store listing by package name
+
+You can use the pname: prefix to search for apps on the Play Store by package name, e.g. by typing "pname:com.google.android.gm" into the search box.
+
+Unfortunately it doesn't always work. In particular it doesn't work for:
+- recently created apps that aren't yet indexed
+- purchased/installed apps unpublished by the developer
+- incompatible or country-restricted apps
+
+Pname+ can still access the Play Store listing in such scenarios.
+
+[<img src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" width="200">](https://play.google.com/store/apps/details?id=app.pname)
---
+++ app/src/main/java/app/pname/MainActivity.java
@@ -0,0 +1,35 @@
+package app.pname;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressWarnings("unused")
+    public void openAppLink(View view) {
+        EditText input = this.findViewById(R.id.input_pname);
+        String packageName = input.getText().toString().trim();
+        Pattern pattern = Pattern.compile("[a-z][a-z0-9_]*(\\.[a-z][a-z0-9_]*)+", Pattern.CASE_INSENSITIVE);
+        if(pattern.matcher(packageName).matches()) {
+            Uri uri = Uri.parse("market://details?id=" + packageName);
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(marketIntent);
+        } else {
+            Toast.makeText(this, R.string.error_invalid, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
